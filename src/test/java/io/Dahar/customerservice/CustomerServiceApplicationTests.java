3820089be0845438de273584c05600ef54e133d8
Ahmed Dahar <DAHAR_AHMED@emsi-edu.ma>
2023-01-25 16:40:30 +0100

    Initial commit

---
+++ src/test/java/io/Dahar/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Dahar.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
